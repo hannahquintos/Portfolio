7982e7a0402f1a21b2d532143b7033f318dce6f2
Hannah Quintos <hannah@quintos.com>
2024-01-30 17:43:54 -0500

    first commit

---
+++ javascript/portfolio.js
@@ -0,0 +1,23 @@
+function openDropdown(){
+  var hamburgerButton = document.getElementById("hamburgerMenu");
+  var dropdownMenu = document.getElementById("dropdownMenu");
+  var header = document.getElementById("header");
+  var closeButton = document.getElementById("closeButton");
+  
+  dropdownMenu.style.display = "block";
+  header.style.backgroundColor = "#949DA9";
+  hamburgerButton.style.display = "none";
+  closeButton.style.display = "block";
+}
+
+function closeDropdown(){
+  var hamburgerButton = document.getElementById("hamburgerMenu");
+  var dropdownMenu = document.getElementById("dropdownMenu");
+  var header = document.getElementById("header");
+  var closeButton = document.getElementById("closeButton");
+  
+  dropdownMenu.style.display = "none";
+  header.style.backgroundColor = "white";
+  hamburgerButton.style.display = "block";
+  closeButton.style.display = "none";
+}
